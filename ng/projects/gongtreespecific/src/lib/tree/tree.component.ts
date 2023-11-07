dbcbdbe3de50d2fe601ec1ea225efd4e8f6ab781
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-11-07 14:05:07 +0100

    simplification

--- ng/projects/gongtreespecific/src/lib/tree/tree.component.ts
+++ ng/projects/gongtreespecific/src/lib/tree/tree.component.ts
@@ -199,14 +199,9 @@ export class TreeComponent implements OnInit {
 
     node.gongNode.IsExpanded = !node.gongNode.IsExpanded
 
-    let buttons = node.gongNode.Buttons
-
     this.gongtreeNodeService.updateNode(node.gongNode, this.GONG__StackPath, this.gongtreeFrontRepoService.frontRepo).subscribe(
       gongtreeNode => {
         console.log("toggleNodeExpansion: updated node")
-        if (buttons) {
-          node.gongNode.Buttons = buttons
-        }
       }
     )
   }
@@ -231,41 +226,29 @@ export class TreeComponent implements OnInit {
   }
 
   onButtonClick(node: FlatNode, button: gongtree.ButtonDB) {
-    let buttons = node.gongNode.Buttons
 
     this.gongtreeButtonService.updateButton(button, this.GONG__StackPath, this.gongtreeFrontRepoService.frontRepo).subscribe(
       gongtreeButton => {
         console.log("button pressed")
-        if (buttons) {
-          node.gongNode.Buttons = buttons
-        }
       }
     )
   }
 
   update(node: FlatNode) {
-    let buttons = node.gongNode.Buttons
 
     node.gongNode.IsInEditMode = false
     this.gongtreeNodeService.updateNode(node.gongNode, this.GONG__StackPath, this.gongtreeFrontRepoService.frontRepo).subscribe(
       gongtreeNode => {
         console.log("node.gongNode.IsInEditMode = false, updated node")
-        if (buttons) {
-          node.gongNode.Buttons = buttons
-        }
       }
     )
   }
 
   onNodeClick(node: FlatNode): void {
-    let buttons = node.gongNode.Buttons
 
     this.gongtreeNodeService.updateNode(node.gongNode, this.GONG__StackPath, this.gongtreeFrontRepoService.frontRepo).subscribe(
       gongtreeNode => {
         console.log("onNodeClick: updated node")
-        if (buttons) {
-          node.gongNode.Buttons = buttons
-        }
       }
     )
   }
